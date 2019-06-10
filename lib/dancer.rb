45b75d4138aa45c0dc4a3fed5808d0aa273804f4
Jessica Hong <jessicahong.ngnr@gmail.com>
2019-06-10 18:10:25 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -8,14 +8,7 @@ class Dancer
   def initialize(name)
     @name = name
   end
-  
-end
 
-class Dancer
-  extend MetaDancing
-end
- 
- class Dancer
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
 end
