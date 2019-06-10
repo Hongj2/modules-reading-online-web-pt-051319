e9478490344a9d46ad04fcf673f395969a9587f5
Jessica Hong <jessicahong.ngnr@gmail.com>
2019-06-10 18:10:57 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,5 +1,6 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+
 class Kid
   include Dance
  
@@ -8,14 +9,4 @@ class Kid
   def initialize(name)
     @name = name
   end
-  
-end
-
-class Kid
-  extend MetaDancing
-end
-
-class Kid
-  extend FancyDance::ClassMethods
-  include FancyDance::InstanceMethods
 end
